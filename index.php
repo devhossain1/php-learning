9766624c25aba80b983a24b66b72ad773d84204a
hossain377 <hossainahmed377@gmail.com>
2020-11-09 00:46:28 +0600

    parvex

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+$stringOne='My email address is ';
+$stringTwo='hossainnstu7@gmail.com';
+$radius=25;
+$pi=3.1416;
+
+//echo $stringOne.$stringTwo;
+//echo $pi*$radius**2;
+//echo $radius++;
+//echo $radius;
+$age=20;
+$age-=10;
+
+echo $age;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>this is the practice</h1>
+</body>
+
+</html>
