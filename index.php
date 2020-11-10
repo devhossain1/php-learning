7b41c73a63e3654136b10884785c4055f6663c51
hossain377 <hossainahmed377@gmail.com>
2020-11-10 10:00:23 +0600

    array

--- index.php
+++ index.php
@@ -1,21 +1,36 @@
 <?php 
-$stringOne='My email address is ';
-$stringTwo='hossainnstu7@gmail.com';
-$radius=25;
-$pi=3.14;
 
-//echo $stringOne.$stringTwo;
-//echo $pi*$radius**2;
-//echo $radius++;
-//echo $radius;
-$age=20;
-//$age-=10;
-//$age*=3;
+//index array
 
-//echo $age;
-//echo floor($pi);
-//echo ceil($pi);
-echo pi();
+$peopleOne=['rahim','karim', 'jaber'];
+//echo $peopleOne[1];
+$peopleTwo=array('hossain','parvez');
+//echo $peopleTwo[1];
+$ages=[20,30,40,50];
+//echo $ages[1];
+//print_r($ages);
+$ages[1]=25;
+//print_r($ages);
+$ages[]=60;
+//print_r($ages);
+array_push($ages, 70);
+//print_r($ages);
+//echo count($ages);
+$peopleThree=array_merge($peopleOne,$peopleTwo);
+//print_r($peopleThree);
+
+
+//asociative array(key and values)
+$ninjasOne=['karim' =>'black','rahim'=>'white','parvez'=>'brown'];
+//echo $ninjasOne['rahim'];
+//print_r($ninjasOne);
+$ninjasTwo=array('mehedi'=>'blue','towfiq'=>'pink');
+//print_r($ninjasTwo);
+$ninjasTwo['imanali']='tal';
+//print_r($ninjasTwo);
+//echo count($ninjasOne);
+$ninjasThree=array_merge($ninjasOne,$ninjasTwo);
+print_r($ninjasThree);
 
 
 ?>
@@ -26,11 +41,11 @@ echo pi();
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>array</title>
 </head>
 
 <body>
-    <h1>this is the practice</h1>
+    <h4>this is the practice</h4>
 </body>
 
 </html>
