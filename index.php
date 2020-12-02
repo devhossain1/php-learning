3a2d28b5ea7bcbd24ae53e56bf094dc72947af46
hossain377 <hossainahmed377@gmail.com>
2020-12-02 20:53:41 +0600

    if/elseif

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
 <?php 
 
-//comparism boolean (true or false)
-//echo true; "1"
-//echo false; ""
+//conditional statement
 
-//numbers
-//echo 5<10;
-//echo 5>10;
-//echo 5==10;
-//echo 5!=10;
-//echo 10==10;
-//echo 5<=5
-//echo 5>=5;    
-//echo 'hossain' < 'parvez';
-// echo 'hossain' > 'parvez';  
-//echo 'hossain' > 'Hossain';    
-//echo 'mario' == 'mario';
-//echo 'mario' == 'Mario';
+//$price = 20;
+//
+//if ($price < 10) {
+//    echo 'The condition is met';
+//}elseif($price<20){
+//    echo 'elseif condition is met';
+//} else{
+//    echo 'the condition is not met';
+//}
 
-//loose vs strict data conversation
-//echo 5 == '5';
-//echo 5 === '5';
-//echo 5===5;
-//echo true=="1";
-echo false=="";
+$products=[
+    ['name'=>'tea','price'=>10],
+    ['name'=>'coffee','price'=>20],
+    ['name'=>'drinks','price'=>60],
+    ['name'=>'soft drink','price'=>80],
+    ['name'=>' cold coffee','price'=>70],
+];
+
+  foreach($products as $product){
+//    if($product['price'] >20 && $product['price']<80){
+//        echo $product['name'] . '<br>';
+//    }
+    
+//     if($product['price'] >60 || $product['price']<20){
+//         echo $product['name'] . '<br>';    }
+//    
+  }
 
 
 ?>
@@ -38,7 +43,15 @@ echo false=="";
 </head>
 
 <body>
-   
+   <ul>
+       <?php foreach($products as $product){ ?>
+           <?php if($product['price']>20){ ?>
+              <li><?php echo $product['name'] . '<br>' ?></li>
+           
+           <?php } ?>
+       
+       <?php }?>
+   </ul>
     
 </body>
 </html>
