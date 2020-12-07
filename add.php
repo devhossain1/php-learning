29b6681bcb641cde6859bb4f15d4a52b5e605c17
hossain377 <hossainahmed377@gmail.com>
2020-12-07 13:12:30 +0600

    Filter & More Validation

--- add.php
+++ add.php
@@ -9,19 +9,28 @@
 if(empty($_POST['email'])){
     echo 'An email is required <br/>';  
 }else{
-   echo htmlspecialchars($_POST['email']); 
+   $email=$_POST['email'];
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ 
+        echo 'email must be a valid email address';
+    }
 }
      //title check
 if(empty($_POST['title'])){
     echo 'A title is required <br/>';  
 }else{
-   echo htmlspecialchars($_POST['title']); 
+   $title=$_POST['title'];
+    if(!preg_match('/^[a-zA-Z\s]+$/',$title)){
+        echo 'The title must be letters and spaces';
+    }
 }
      //the ingredients check
 if(empty($_POST['ingredients'])){
     echo 'An email is required <br/>';  
 }else{
-   echo htmlspecialchars($_POST['ingredients']); 
+   $ingredients=$_POST['ingredients'];
+    if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+        echo 'ingredients must be a comma separated list';
+    }
 }
      
  };
